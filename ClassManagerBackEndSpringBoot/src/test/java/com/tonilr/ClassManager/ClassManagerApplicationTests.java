3d5b145d1937a1394a22c6cf623e00210a1deded
ToniLR999 <tonilupianezroman@gmail.com>
2024-08-04 11:35:25 +0200

    Spring boot project

---
+++ ClassManagerBackEndSpringBoot/src/test/java/com/tonilr/ClassManager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonilr.ClassManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
