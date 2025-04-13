71aeeabc31ce75760460246ccfc767358cd90379
Toni <tonilupianezroman@gmail.com>
2025-04-13 12:54:25 +0200

    Solved errors con compile

--- ClassManagerFrontEndAngular/src/app/app.module.ts
+++ ClassManagerFrontEndAngular/src/app/app.module.ts
@@ -1,47 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatOptionModule } from '@angular/material/core'; // Opcional, a veces se importa directamente en MatSelectModule
-import { MatIconModule } from '@angular/material/icon';
+import { AuthModule } from './auth/auth-module';
 import { DashboardComponent } from './dashboard/dashboard-component/dashboard-component.component';
-import { AuthGuard } from './auth/auth.guard';
-import { AuthService } from './auth/auth.service';
-import { RouterModule } from '@angular/router';
-import { MatInputModule } from '@angular/material/input';           // <-- If you're using matInput as well
-import { MatButtonModule } from '@angular/material/button'; 
-
-const routes = [
-  { path: '', component: AppComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    DashboardComponent
   ],
   imports: [
-    MatSelectModule,
-    MatOptionModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatIconModule,
-    RouterModule.forRoot(routes)
+    BrowserAnimationsModule,
+    AuthModule,
+    HttpClientModule
   ],
-  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- ClassManagerFrontEndAngular/src/app/auth/auth-module.ts
+++ ClassManagerFrontEndAngular/src/app/auth/auth-module.ts
@@ -1,35 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar'; 
+
 import { LoginComponent } from './login-component/login-component.component';
 import { RegisterComponent } from './register-component/register-component.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app-routing.module';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule,
-    RouterModule,
+
+    MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatFormFieldModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        AppRoutingModule,
-        MatIconModule,
+    MatSnackBarModule
+
   ]
 })
-export class AuthModule {}
+export class AuthModule { }
