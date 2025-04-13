2f8da0a4de0babe718869a7dc4d6d782e3af1612
ToniLR999 <tonilupianezroman@gmail.com>
2025-04-13 23:34:54 +0200

    Dashboard services

--- ClassManagerFrontEndAngular/src/app/dashboard/students/students.component.ts
+++ ClassManagerFrontEndAngular/src/app/dashboard/students/students.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.scss'],
 })
 export class StudentsComponent implements OnInit {
+  students: any[] = [];
 
-  constructor() { }
+  newStudent = {
+    name: '',
+    email: ''
+  };
+
+  editMode = false;
+  editStudentId: number | null = null;
+  editStudent = {
+    name: '',
+    email: ''
+  };
+
+  constructor(private studentService: StudentService) {}
 
   ngOnInit(): void {
+    this.loadStudents();
   }
 
+  loadStudents() {
+    this.studentService.getAllStudents().subscribe(data => {
+      this.students = data;
+    });
+  }
+
+  createStudent() {
+    if (!this.newStudent.name || !this.newStudent.email) return;
+    this.studentService.createStudent(this.newStudent).subscribe(() => {
+      this.newStudent = { name: '', email: '' };
+      this.loadStudents();
+    });
+  }
+
+  startEdit(student: any) {
+    this.editMode = true;
+    this.editStudentId = student.id;
+    this.editStudent = { name: student.name, email: student.email };
+  }
+
+  updateStudent() {
+    if (!this.editStudentId) return;
+    this.studentService.updateStudent(this.editStudentId, this.editStudent).subscribe(() => {
+      this.cancelEdit();
+      this.loadStudents();
+    });
+  }
+
+  cancelEdit() {
+    this.editMode = false;
+    this.editStudentId = null;
+    this.editStudent = { name: '', email: '' };
+  }
+
+  deleteStudent(id: number) {
+    if (confirm('¿Eliminar alumno?')) {
+      this.studentService.deleteStudent(id).subscribe(() => {
+        this.loadStudents();
+      });
+    }
+  }
 }
