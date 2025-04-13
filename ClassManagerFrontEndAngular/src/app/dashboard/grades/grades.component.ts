671968c4c9ea507f7211802330a94a810e98da80
ToniLR999 <tonilupianezroman@gmail.com>
2025-04-13 23:42:30 +0200

    Grades crud

--- ClassManagerFrontEndAngular/src/app/dashboard/grades/grades.component.ts
+++ ClassManagerFrontEndAngular/src/app/dashboard/grades/grades.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { GradeService } from 'src/app/dashboard/grades/grades.service';
+import { StudentService } from 'src/app/dashboard/students/students.service';
 
 @Component({
   selector: 'app-grades',
   templateUrl: './grades.component.html',
-  styleUrls: ['./grades.component.css']
+  styleUrls: ['./grades.component.scss'],
 })
 export class GradesComponent implements OnInit {
+  grades: any[] = [];
+  students: any[] = [];
 
-  constructor() { }
+  newGrade = {
+    studentId: null,
+    subject: '',
+    score: null
+  };
+
+  editMode = false;
+  editGradeId: number | null = null;
+  editGrade = {
+    subject: '',
+    score: null
+  };
+
+  constructor(
+    private gradeService: GradeService,
+    private studentService: StudentService
+  ) {}
 
   ngOnInit(): void {
+    this.loadGrades();
+    this.studentService.getAllStudents().subscribe(data => {
+      this.students = data;
+    });
   }
 
+  loadGrades() {
+    this.gradeService.getAllGrades().subscribe(data => {
+      this.grades = data;
+    });
+  }
+
+  createGrade() {
+    this.gradeService.createGrade(this.newGrade).subscribe(() => {
+      this.newGrade = { studentId: null, subject: '', score: null };
+      this.loadGrades();
+    });
+  }
+
+  startEdit(grade: any) {
+    this.editMode = true;
+    this.editGradeId = grade.id;
+    this.editGrade = { subject: grade.subject, score: grade.score };
+  }
+
+  updateGrade() {
+    if (!this.editGradeId) return;
+    this.gradeService.updateGrade(this.editGradeId, this.editGrade).subscribe(() => {
+      this.cancelEdit();
+      this.loadGrades();
+    });
+  }
+
+  cancelEdit() {
+    this.editMode = false;
+    this.editGradeId = null;
+    this.editGrade = { subject: '', score: null };
+  }
+
+  deleteGrade(id: number) {
+    if (confirm('¿Eliminar calificación?')) {
+      this.gradeService.deleteGrade(id).subscribe(() => {
+        this.loadGrades();
+      });
+    }
+  }
 }
--- ClassManagerFrontEndAngular/src/app/dashboard/grades/grades.service.ts
+++ ClassManagerFrontEndAngular/src/app/dashboard/grades/grades.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class GradesService {
+export class GradeService {
+  private apiUrl = `${environment.apiUrl}/grades`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllGrades(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getGradesByStudentId(studentId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/student/${studentId}`);
+  }
+
+  createGrade(gradeData: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, gradeData);
+  }
+
+  updateGrade(id: number, gradeData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, gradeData);
+  }
+
+  deleteGrade(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
