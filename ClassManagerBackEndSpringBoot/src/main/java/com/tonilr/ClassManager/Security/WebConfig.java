3c41058d86f0a6540e1c4065f0607720f3ed4773
Toni <tonilupianezroman@gmail.com>
2025-08-10 21:36:17 +0200

    updated cors policy

--- ClassManagerBackEndSpringBoot/src/main/java/com/tonilr/ClassManager/Security/WebConfig.java
+++ ClassManagerBackEndSpringBoot/src/main/java/com/tonilr/ClassManager/Security/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://myclassmanager.netlify.app"
+                    )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
