c6bfab4734696233b11b18372ab06a73bba2d59f
Toni <tonilupianezroman@gmail.com>
2025-08-10 19:36:36 +0200

    Fix: healthcheck failures on railway deploy

---
+++ ClassManagerBackEndSpringBoot/src/main/java/com/tonilr/ClassManager/Controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.tonilr.ClassManager.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
